77acc052adc2ad3718efd4495a0f17d148a88824
mikeg64 <mikeg64@993b1211-408a-0410-8964-a8528df93632>
2016-10-03 10:06:26 +0000

    Version used for benchtests re. iceberg/sharc University of Sheffield October 2016

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 #$ -l h_rt=99:00:00
 #$ -cwd
-#$ -l arch=intel*
-#$ -l mem=6G
+#$ -l arch=intel-x5650
+#$ -l rmem=6G
 
 #$ -N S2D
 
 
 
-module add compilers/pgi/14.4
+module add compilers/intel/15.0.3
 
+export TIMECOUNTER=0
 
 echo Starting vac now.
+source timeused
 ./vac < vac.par
+source timeused
 
 
